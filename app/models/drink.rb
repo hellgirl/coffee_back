93189f13ff007b60f02ca55e5e0a32e198045a3f
Kate Bayshihina <katya.bayshihina@kandasoft.com>
2018-04-07 17:08:18 +0300

    fix issue with drink deletion

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,5 +1,5 @@
 class Drink < ApplicationRecord
-  has_many :drinks_volumes
+  has_many :drinks_volumes, dependent: :destroy
   has_many :volumes, through: :drinks_volumes
 
   mount_uploader :coffee_image, ::CoffeeImageUploader
