1cbf881a5d9a887834e6ebf7994821f80a02f823
Bayshihina <e.bayshihina@gmail.com>
2018-08-04 18:01:48 +0300

    #158478508 don't add a new card if it already exists

--- app/services/payment_processor.rb
+++ app/services/payment_processor.rb
@@ -10,12 +10,17 @@ class PaymentProcessor
     order.create_payment(yandex_meta: @data)
     user = order.user
     payment_method_id = @data["payment_method"]["id"]
-    card = user.payment_cards.where(payment_method_id: payment_method_id)
-    if card.blank?
+    if card_not_exists?(user, @data["payment_method"]["card"])
       user.payment_cards.create(card_meta: @data["payment_method"]["card"], payment_method_id: payment_method_id,
                                 title: @data["payment_method"]["title"], default: true)
     end
     return unless @data["paid"]
     order.update(status: Order.statuses[:paid])
+  end
+
+  private
+
+  def card_not_exists?(user, meta)
+    user.payment_cards.pluck(:card_meta).none? { |card_meta| card_meta == meta }
   end
 end
